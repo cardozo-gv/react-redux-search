5f9d45b33ff4cfb9f76aa1fcb80b2ca5669b85e0
Your Name <griseldavcardozo@gmail.com>
2019-07-07 19:25:02 -0300

    Updates

---
+++ precache-manifest.e1170cea93efdf10e1a1424805c80012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2401747d2aea22ba7f0",
+    "url": "/react-redux-search/static/css/main.90eebae6.chunk.css"
+  },
+  {
+    "revision": "b2401747d2aea22ba7f0",
+    "url": "/react-redux-search/static/js/main.b2401747.chunk.js"
+  },
+  {
+    "revision": "2d1b8f0ed1766d6b44c3",
+    "url": "/react-redux-search/static/js/runtime~main.2d1b8f0e.js"
+  },
+  {
+    "revision": "c763be8c5157845e817a",
+    "url": "/react-redux-search/static/js/2.c763be8c.chunk.js"
+  },
+  {
+    "revision": "e10f39cbc7c9602061313c929a84291c",
+    "url": "/react-redux-search/index.html"
+  }
+];
